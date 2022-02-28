be3227a0f25302c63ea4672df009b9259cf678f4
Fülöp Tibor <fuloptibi03@gmail.com>
2022-02-28 22:09:34 +0200

    Webpack changes for deployment compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Chirp',
-          template: './src/index.html'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'roms', to: 'roms' }
-            ]
-        })
-    ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    watch: true,
+module.exports = (env, argv) => {
+    return {
+        mode: argv.mode || 'development',
+        entry: './src/index.js',
+        plugins: [
+            new HtmlWebpackPlugin({
+              title: 'Chirp',
+              template: './src/index.html'
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'roms', to: 'roms' }
+                ]
+            })
+        ],
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true
+        },
+        watch: argv.mode === "development" ? true : false,
+    }
 };
