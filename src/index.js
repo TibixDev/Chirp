6c75858528e982b08773565315abfbece8a00814
TibixDev <fuloptibi03@gmail.com>
2022-10-23 23:04:04 +0300

    ROM selection improvements, P2P removed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { Display } from "./display";
 import { Keyboard } from "./keyboard";
 import { EmuDefault } from "./defaultprops";
 import { Log } from "./logger";
-import Peer from 'peerjs';
 
 window.Log = Log;
 const keyboard = new Keyboard(EmuDefault.keyboard, EmuDefault.keyboardKeys);
@@ -15,8 +14,10 @@ const emulator = new Emulator(display, keyboard, "./roms/airplane.rom", false);
 window.emulator = emulator;
 
 const romLink = document.getElementById("romLink");
-romLink.addEventListener("input", () => {
+romLink.addEventListener("change", () => {
+    console.log("ooo")
     emulator.ChangeROM(romLink.value);
+    emulator.Reset();
 });
 
 const romFile = document.getElementById("romFile");
@@ -46,31 +47,3 @@ const resetBtn = document.getElementById("resetBtn");
 resetBtn.addEventListener("click", () => {
     emulator.Reset();
 });
-
-// P2P Stuff
-const peerID = `chirp-${Math.floor(Math.random() * 10000)}`;
-const peer = new Peer(peerID);
-console.log("Peer ID: " + peerID);
-
-let peerConn = null;
-peer.on('connection', (conn) => {
-    conn.on('data', (data) => {
-        console.log(data);
-    });
-    conn.on('open', () => {
-        conn.send('hello!');
-        peerConn = conn;
-        window.peerConn = peerConn;
-    });
-});
-
-function connect(id) {
-    const conn = peer.connect(`chirp-${id}`);
-    conn.on('open', () => {
-      conn.send('hi!');
-    });
-    return conn;
-}
-
-window.peer = peer;
-window.connect = connect;
